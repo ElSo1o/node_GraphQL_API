08b8e80ce6fe7ab4e372998f07a48007f0cac5b8
ElSolo <ElSolo1991@mail.ru>
2018-06-16 00:03:47 +0300

    add gql for project

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+let root = {
+    hello: () => {
+        return 'Hello ElSolo on GraphQL!';
+    },
+};
 
 const app = express();
 
@@ -21,11 +34,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
 
 // error handler
 app.use(function(err, req, res, next) {
