8fe02f001ff0874a1926e41d41fe9585257c3fa3
ElSolo <ElSolo1991@mail.ru>
2018-06-22 00:17:42 +0300

    add db connection

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const graphqlHTTP = require('express-graphql');
-// const { buildSchema } = require('graphql');
+const MongoClient = require('mongodb').MongoClient;
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const {schema, root} = require('./graphQL/index')
 const app = express();
 
+const assert = require('assert');
+
+// Connection url
+const url = 'mongodb://localhost:27017';
+// Database Name
+const dbName = 'ElSoloDb';
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -30,14 +36,27 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true,
 }));
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 
+MongoClient.connect(url, (err, client) => {
+    if (err) {
+        throw err;
+    }
+    const db = client.db(dbName);
+    db.collection('users').find().toArray((err, result) => {
+        if (err) {
+            throw err;
+        }
+        console.log(result);
+    });
+    client.close()
+});
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
