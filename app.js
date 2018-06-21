9b6f3fa8eb818b0aa8fb21f7251a86aafd54468c
ElSolo <ElSolo1991@mail.ru>
2018-06-22 02:38:36 +0300

    add jwt for project

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const graphqlHTTP = require('express-graphql');
 const MongoClient = require('mongodb').MongoClient;
+const jwt = require('express-jwt')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const {schema, root} = require('./graphQL/index')
+const mongoose = require('mongoose')
 const app = express();
 
 const assert = require('assert');
@@ -30,6 +32,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use('/graphql', jwt({
+    secret: 'shhhhhhared-secret',
+    requestProperty: 'auth',
+    credentialsRequired: false,
+}));
+
+
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     rootValue: root,
@@ -39,7 +48,13 @@ app.use('/graphql', graphqlHTTP({
 app.use((req, res, next) => {
   next(createError(404));
 });
+function loggingMiddleware(req, res, next) {
+    console.log('ip:', req.ip);
+    next();
+}
 
+
+app.use(loggingMiddleware);
 
 MongoClient.connect(url, (err, client) => {
     if (err) {
--- graphQL/index.js
+++ graphQL/index.js
@@ -1,32 +1,79 @@
 const { buildSchema } = require('graphql');
 exports.schema = buildSchema(`
-  type Query {
-    hello: String
-    quoteOfTheDay: String
-    random: Float!
-    rollThreeDice: [Int],
-    rollDice(numDice: Int!, numSides: Int): [Int]
-  }
-`);
+      type Query {
+        post(_id: String): Post
+        posts: [Post]
+        comment(_id: String): Comment
+      }
+      type Post {
+        _id: String
+        title: String
+        content: String
+        comments: [Comment]
+      }
+      type Comment {
+        _id: String
+        postId: String
+        content: String
+        post: Post
+      }
+      type Mutation {
+        createPost(title: String, content: String): Post
+        createComment(postId: String, content: String): Comment
+      }
+      schema {
+        query: Query
+        mutation: Mutation
+      }
+    `);
+
+const prepare = (o) => {
+    o._id = o._id.toString()
+    return o
+}
 
 exports.root = {
-    hello: () => {
-        return 'Hello ElSolo on GraphQL!';
-    },
-    quoteOfTheDay: () => {
-        return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
-    },
-    random: () => {
-        return Math.random();
-    },
-    rollThreeDice: () => {
-        return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
-    },
-    rollDice: function ({numDice, numSides}) {
-        let output = [];
-        for (let i = 0; i < numDice; i++) {
-            output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    Query: {
+        post: async (root, {_id}) => {
+            return prepare(await Posts.findOne(ObjectId(_id)))
         }
-        return output;
-    }
-};
+    },
+    Post: {
+        comments: async ({_id}) => {
+            return (await Comments.find({postId: _id}).toArray()).map(prepare)
+        }
+    },
+    Comment: {
+        post: async ({postId}) => {
+            return prepare(await Posts.findOne(ObjectId(postId)))
+        }
+    },
+    Mutation: {
+        createPost: async (root, args, context, info) => {
+            const res = await Posts.insert(args)
+            return prepare(await Posts.findOne({_id: res.insertedIds[1]}))
+        },
+        createComment: async (root, args) => {
+            const res = await Comments.insert(args)
+            return prepare(await Comments.findOne({_id: res.insertedIds[1]}))
+        },
+    },
+}
+
+// exports.root = {
+//     hello: async () => {
+//         return await 'Hello Elsolo';
+//     },
+//     rollDice: function ({numDice, numSides}) {
+//         let output = [];
+//         for (let i = 0; i < numDice; i++) {
+//             output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+//         }
+//         return output;
+//     }
+// };
+
+// const schema = makeExecutableSchema({
+//     typeDefs,
+//     resolvers
+// })
