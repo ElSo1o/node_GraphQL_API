34eeadf9e8fd4ba931c2680d2a92a9493bcb18bf
ElSolo <ElSolo1991@mail.ru>
2018-07-05 16:27:52 +0300

    rename db connection

--- db/collections.js
+++ db/collections.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const url = 'mongodb://localhost:27017';
+/***dev**/
 const dbName = 'ElSoloDb';
-
+/***production**/
+// const dbName = 'elsolodb';
 mongoose.connect(`${url}/${dbName}`);
 
 exports.Cat = mongoose.model('cats', { name: String });
