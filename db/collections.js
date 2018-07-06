a90afc5140e30a2f1716cd6e8154950c7473fd7a
ElSolo <ElSolo1991@mail.ru>
2018-07-06 23:28:30 +0300

    heroku push

--- db/collections.js
+++ db/collections.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
+const {params} = require('./params')
 // const url = 'mongodb://ElSolo:Inbeatwetrust1710919856@ds227481.mlab.com:27481';
 /***dev**/
 // const dbName = 'ElSoloDb';
 // const url = 'mongodb://localhost:27017';
 /***production**/
-const url = 'mongodb://ElSolo:Inbeatwetrust1710919856@ds227481.mlab.com:27481';
-const dbName = 'elsolodb';
+const url = params.url || 'mongodb://localhost:27017';
+const dbName = params.dbName ||  'ElSoloDb';
 mongoose.connect(`${url}/${dbName}`);
 
 exports.Cat = mongoose.model('cats', { name: String });
---
+++ db/params.js
@@ -0,0 +1,4 @@
+exports.params = {
+    url: 'mongodb://ElSolo:Inbeatwetrust1710919856@ds227481.mlab.com:27481',
+    dbName: 'elsolodb'
+}
