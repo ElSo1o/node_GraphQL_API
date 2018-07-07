006e0927280392b5f9f4688fc60dd14b5af4b882
ElSolo <ElSolo1991@mail.ru>
2018-07-07 12:39:16 +0300

    remove params module

--- db/collections.js
+++ db/collections.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose')
-const {params} = require('./params')
-// const url = 'mongodb://ElSolo:Inbeatwetrust1710919856@ds227481.mlab.com:27481';
 /***dev**/
-// const dbName = 'ElSoloDb';
-// const url = 'mongodb://localhost:27017';
-/***production**/
-const url = params.url || 'mongodb://localhost:27017';
-const dbName = params.dbName ||  'ElSoloDb';
+const url = 'mongodb://localhost:27017';
+const dbName = 'ElSoloDb';
 mongoose.connect(`${url}/${dbName}`);
 
 exports.Cat = mongoose.model('cats', { name: String });
--- db/params.js
+++
@@ -1,4 +0,0 @@
-exports.params = {
-    url: 'mongodb://ElSolo:Inbeatwetrust1710919856@ds227481.mlab.com:27481',
-    dbName: 'elsolodb'
-}
